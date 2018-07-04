cbb1089361869b03409a355caa98583d6e4e2bfe
Aryailia <Aryailia@users.noreply.github.com>
2018-07-05 04:56:49 +0800

    .

--- .config/scripts/launcher-bookmark.sh
+++ .config/scripts/launcher-bookmark.sh
@@ -15,30 +15,32 @@
 # eg. "#japanese #grammar"  can be found with "#gr #jp"
 #
 # Control flow of is determined by type of arguments passed:
-# 0) NOTE: May want to run this with: ASDF=0 $0
+# 0) NOTE: May want to run this with: SEARCH_TYPE=0 $0
 # 1) No arguments or pre-set variables inits rofi
-# 2) ASDF is preset but no arguments, populates rofi depending on ASDF
+# 2) SEARCH_TYPE is preset but no arguments, changes how rofi menu is populated
 # 3) Single bookmark: <data> <id>, ID is its line number in the file
 #    
 # 4) Has browser and 
 
 # TODO: Fix commenting and documentation
-# TODO: rofi menu include all the types (quickfix)
 # TODO: Add to i3
-# TODO: 
+# TODO: Maybe add mozilla/netscape's bookmark.xml parsing?
+# TODO: Need to do something prompt-browser process blocking UI (synchronous)
+#       delete from bottom of file too when completed task
+# TODO: Help file
 
 # Tightly-coupled-to-environment file location dependencies
-scripts="$HOME/dotfiles/.config/scripts"
+subfolder="./helpers"
 bookmarks="$HOME/ime/bookmarks.txt"
-promptbrowser="$scripts/prompt-browser"
+promptbrowser="./prompt-browser"
 
 # Creates the intermediary files for parameter passing for rofi script mode
 create_populator() {
-  filename="$scripts/bookmark-$1.sh"
+  filename="$subfolder/bookmark-$1.sh"
   content=$(printf '%s\n%s\n%s' \
     "#!/bin/sh" \
     "# please look to the rofi-prompt-bookmarks.sh for more" \
-    "ASDF=$1 $0 \"\$*\"")
+    "SEARCH_TYPE=\"$1\" \"$0\" \"\$*\"")
 
   # Save on a disk write if possible
   [ "$(cat "$filename")" != "$content" ] && printf '%s' "$content" > "$filename"
@@ -46,14 +48,22 @@ create_populator() {
   shellcheck "$filename"
 }
 
-extract_entry_andor_id() {
+# Gets the desired column and the line number (introduced by awk)
+extract_entry_and_id() {
   case "$1" in
-    # First rofi menu calls this part through populator, echos bookmarks
-    # Tabular formating for my bookmarks
-    # Digit at the end added by awk
-    title) printf '%s' 's/^| *\(.*\)| *#.*|\([0-9]*\)$/\1 \2/' ;;
-    tags)  printf '%s' 's/^.*| *\(#.*\)| *http.*| *\([0-9]*\)$/\1 \2/' ;;
-    url)   printf '%s' 's/^.*| *\(http.*\)|\([0-9]*\)$/\1 \2/' ;;
+    title) cat - | sed 's/^| *\(.*\)| *#.*|\([0-9]*\)$/\1 \2/' ;;
+    tags)  cat - | sed 's/^.*| *\(#.*\)| *http.*| *\([0-9]*\)$/\1 \2/' ;;
+    url)   cat - | sed 's/^.*| *\(http.*\)|\([0-9]*\)$/\1 \2/' ;;
+    #url)   printf '%s' 's/^.*| *\(http.*\)|\([0-9]*\)$/\1 \2/' ;;
+    *)
+      # If using different shell could do {cat - | tee >() >() >()}
+      # Storing in variable has problem of maybe running out of memory
+      # Which may be a concern for very large bookmark files
+      # Temp files is next best option for dash, though there are others
+      stdin="$(cat -)"
+      printf '%s\n' "$stdin" | extract_entry_and_id title
+      printf '%s\n' "$stdin" | extract_entry_and_id tags
+      printf '%s\n' "$stdin" | extract_entry_and_id url
   esac
 }
 
@@ -62,24 +72,29 @@ extract_entry_andor_id() {
 # with previous choice as the parameter. Any stdout will populate the menu
 if [ -z "$*" ]; then
   # 1) Initial run, no arguments
-  if [ -z "$ASDF" ]; then
+  if [ -z "$SEARCH_TYPE" ]; then
     # Because cannot pass arguments to script mode rofi, have to create shell
     # script files that cal
     # haveCreate three files to basically call pass
     create_populator title
     create_populator tags
     create_populator url
+    create_populator all
 
-    rofi -show url -modi 'url:bookmark-url.sh'
+    title="TITLE:$subfolder/bookmark-title.sh"
+    tags="TAGS:$subfolder/bookmark-tags.sh"
+    url="URL:$subfolder/bookmark-url.sh"
+    all="/:$subfolder/bookmark-all.sh" # 'all' handled a bit differently 
+    rofi -show '/' -modi "$all,$title,$url,$tags" -matching fuzzy
 
-  # ASDF=<populator-type> $0
+  # SEARCH_TYPE=<populator-type> $0
   # 2) Called by rofi 
   else
     awk <"$bookmarks" '
       {$0=$0""NR}        # append row number (to after the table last bar)
       /^\/\//{next}      # skip over C-like comments
       !/^[0-9]*$/{print} # skip blanks lines (awk added row number)
-    ' |sed "$(extract_entry_andor_id "$ASDF")"
+    ' |extract_entry_and_id "$SEARCH_TYPE"
   fi
 
 # After a URL is entered
@@ -87,7 +102,7 @@ if [ -z "$*" ]; then
 elif printf '%s' "$*" | grep -q '^.\+ \+[0-9]\+$'; then
   id="$*"
   id="${id##*\ }"
-  url="$(sed <"$bookmarks" "${id}q;d" | sed "$(extract_entry_andor_id url)")"
+  url="$(sed <"$bookmarks" "${id}q;d" | extract_entry_and_id url)"
   url="${url%\ *}"
 
   # Append the url to all the browser_tags to pass both to the fourth step
@@ -99,18 +114,7 @@ elif printf '%s' "$*" | grep -q '^.\+ \+[0-9]\+$'; then
 else
   args="$*"
   url=$(printf '%s' "$args" | awk '{print $2}') # select second and trim
+  # TODO: Need to do something about this blocking rofi synchronously
+  #       Crash when midori is first started 
   "$promptbrowser" "${args%%\ *}" "$url"
 fi
-exit 0
-
-
-title='TITLE:bookmark-title.sh'
- tags='TAGS :bookmark-tags.sh'
-  url='URL:bookmark-url.sh'
-
-  choice=$(rofi -show 'URL' -modi "$url" -matching 'fuzzy')
-  #-config $HOME/.config/rofi/config
-  #rofi -show keys
-# choice="$(rofi -show combi -combi-modi "$tags,$title,$url" \
-#   -modi "combi,$title,$url,$tags" -matching fuzzy)"
-[ -z $choice ] && exit 1
