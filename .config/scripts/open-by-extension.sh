62b2f3986d9e7e41f7c2b7cc96f8f0684d24d94d
Aryailia <Aaryailia@users.noreply.github.com>
2018-08-09 03:01:13 +0800

    a lot of changes that i dont really remember cause laptop died iunno

--- .config/scripts/open-by-extension.sh
+++ .config/scripts/open-by-extension.sh
@@ -1,22 +1,20 @@
 #!/usr/bin/env sh
+  #
 
 # Dependencies
 browseropen="browser-fzf.sh"
 
 url=$1
-extension=${url##*.}
+extension=$(printf '%s' "${url##*.}"  | awk '{print tolower($0)}')
 
 match() {
   printf '%s' "$1" | grep -q "$2"
 }
 
-if match "$extension" 'png\|jpg\|jpeg'; then
-  nohup setsid feh "$url" >/dev/null
-elif match "$extension" 'mkv\|mp4\|gif\|web'; then
-  nohup setsid mpv -quiet "$url" >/dev/null
-elif match "$extension" 'mp3\|flac\|opus\|pdf'; then
-  nohup setsid mpv -quiet "$url" >/dev/null
-else
-  "$browseropen" "${url}: " "$url"
-fi
+case "$extension" in
+  png|jpg|jpeg) nohup setsid feh "$url" >/dev/null ;;
+  mkv|mp4|gif|web) nohup setsid mpv -quiet "$url" >/dev/null ;;
+  mp3|flac|opus|pdf) nohup setsid mpv -quiet "$url" >/dev/null ;; 
+  *) "$browseropen" "${url}: " "$url"
+esac
 
