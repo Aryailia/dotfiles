53d84dccdd211b9a0f7dc2b9c8be735f5fbf3c79
Aryailia <Aryailia@users.noreply.github.com>
2018-08-30 11:52:19 +0800

    add semicolon to pane switch for windows vm, change to rebase and to use -C flag for git fetching

--- install/fetch-vim-plugins.sh
+++ install/fetch-vim-plugins.sh
@@ -1,35 +1,39 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
-# Prompt with default 
-echo 'Enter path to vim config'
-printf '(Leave blank for "$HOME/.vim"): '
-read -r vimconfig
-vimconfig="${vimconfig:-$HOME/.vim}"
+main() {
+  # Prompt with default 
+  echo 'Enter path to vim config'
+  printf '(Leave blank for "$HOME/.vim"): '
+  read -r vimconfig
+  vimconfig="${vimconfig:-$HOME/.vim}"
+
+  mkdir -p "$vimconfig/autoload"
+
+  # Unlike the plugins, do not update pathogen, discrepancy cause no reason...
+  if [ ! -f "$vimconfig/autoload/pathogen.vim" ]; then
+    wget 'https://tpo.pe/pathogen.vim' -P "$vimconfig/autoload"
+  fi
+
+  [ ! -d "$vimconfig/bundle" ] && 'mkdir' "$vimconfig/bundle"
+  cloneupdate "vimwiki" "vimwiki"
+  cloneupdate "godlygeek" "tabular"
+  cloneupdate "tpope" "vim-sleuth"
+  cloneupdate "tpope" "vim-surround"
+  #cloneupdate "tpope" "vim-markdown"
+
+  cloneupdate "rust-lang" "rust.vim"
+  #cloneupdate "plasticboy" "vim-markdown"
+  #cloneupdate "vim-pandoc" "vim-markdownfootnotes"
+  cloneupdate "mzlogin" "vim-markdown-toc"
+}
 
 cloneupdate() {
   target="$vimconfig/bundle/$2"
   if [ -d "$target" ]; then
-    git --git-dir="$target/.git" pull origin master 
+    git -C "$target" rebase origin/master 
   else
     git clone "https://github.com/$1/$2.git" "$target"
   fi
 }
 
-mkdir -p "$vimconfig/autoload"
-
-# Unlike the plugins, do not update pathogen, discrepancy cause no reason...
-if [ ! -f "$vimconfig/autoload/pathogen.vim" ]; then
-  wget 'https://tpo.pe/pathogen.vim' -P "$vimconfig/autoload"
-fi
-
-[ ! -d "$vimconfig/bundle" ] && 'mkdir' "$vimconfig/bundle"
-cloneupdate "vimwiki" "vimwiki"
-cloneupdate "godlygeek" "tabular"
-cloneupdate "tpope" "vim-sleuth"
-cloneupdate "tpope" "vim-surround"
-#cloneupdate "tpope" "vim-markdown"
-
-cloneupdate "rust-lang" "rust.vim"
-#cloneupdate "plasticboy" "vim-markdown"
-#cloneupdate "vim-pandoc" "vim-markdownfootnotes"
-cloneupdate "mzlogin" "vim-markdown-toc"
+main
