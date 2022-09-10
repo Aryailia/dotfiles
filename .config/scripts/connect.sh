5f3610b20775f396f2936f1473e3f195ef56fb79
Aryailia <Aryailia@users.noreply.github.com>
2022-09-10 18:30:44 +0400

    Cleanup tab completion logic

--- .config/scripts/connect.sh
+++ .config/scripts/connect.sh
@@ -12,45 +12,47 @@ ADDR="${DOTENVIRONMENT}/addr.csv"
 SSH_DIR="${HOME}/.ssh"
 
 
-_CMDS="${_CMDS}help${NL}"
+_HOSTS="help${NL}"
 _help() {
   printf %s\\n "SYNOPSIS" >&2
-  printf %s\\n "  ${NAME} <defined-host>" >&2
-  printf %s\\n "  ${NAME} <ssh-uri>" >&2
+  printf %s\\n "  ${NAME} [<defined-host>]" >&2
+  printf %s\\n "  ${NAME} <defined-host> [arg ...]" >&2
+  printf %s\\n "  ${NAME} <ssh-uri> [arg ...]" >&2
   printf %s\\n "  ${NAME} help" >&2
 
   printf %s\\n "" "DESCRIPTION" >&2
   printf %s\\n "  Connects to the nickname host, defined in '${ADDR}'." >&2
-  printf %s\\n "  Or connects to <ssh-uri> and displays a menu of SSH keys." >&2
+  printf %s\\n "  Or connects to <ssh-uri>, prompting the user with an fzf" >&2
+  printf %s\\n "  menu for which SSH private key they want to use." >&2
+  printf %s\\n "  Extra args are passed as is to the ssh command." >&2
   exit 1
 }
 
-[ -r "${ADDR}" ] &&_CMDS="${_CMDS}$( <"${ADDR}" cut -f1 -d',' )${NL}"
+[ -r "${ADDR}" ] && _HOSTS="${_HOSTS}$( <"${ADDR}" cut -f1 -d',' )${NL}"
 
 #run: sh % website
 main() {
-  [ "${_CMDS}" != "${_CMDS#help"${NL}"*help"${NL}"}" ] \
+  [ "${_HOSTS}" != "${_HOSTS#help*"${NL}help${NL}"}" ] \
     && die FATAL 1 "There is a host with help as a hostname in '${ADDR}'"
-  cmd="${1}"; [ "$#" -gt 0 ] && shift 1
+  [ "${1}" != "${1#*,}" ] && die FATAL 1 "The \$1 <ssh-uri> '${1}' has a comma"
 
-  if    [ "${cmd}" = 'help' ]; then
+  case "${1}"
+  in help)
     _help
-
-  elif  [ "${cmd}" != "${cmd#*,}" ]; then
-    die FATAL 1 "The <ssh-uri> '${cmd}' has a comma"
-
-  elif  [ "${_CMDS}" != "${_CMDS#*"${NL}${cmd}${NL}"}" ]; then
-    connect_to_addr_host "${cmd}" "$@"
-
-  elif [ -z "${cmd}" ]; then
-    choice="$( printf %s "${_CMDS}" | fzf )" || exit "$?"
+  ;; "")
+    choice="$( printf %s "${_HOSTS}" | fzf )" || exit "$?"
     [ "${choice}" = "help" ] && _help
     connect_to_addr_host "${choice}" "$@"
-
-  else
-    private_key="$( list_private_keys | fzf )" || exit "$?"
-    ssh -i "${private_key}" "$@" "${cmd}"
-  fi
+  ;; *)
+    cmd="${1}"; shift 1
+    if [ "${_HOSTS}" != "${_HOSTS#*"${NL}${cmd}${NL}"}" ]; then
+      connect_to_addr_host "${cmd}" "$@"
+    else
+      connect_to_addr_host "${cmd}" "$@"
+      private_key="$( list_private_keys | fzf )" || exit "$?"
+      ssh -i "${private_key}" "$@" "${1}"
+    fi
+  esac
 }
 
 connect_to_addr_host() {
@@ -75,29 +77,20 @@ list_private_keys() {
 
 
 if [ -n "${COMP_LINE}" ]; then  # `complete` is non-POSIX, so a bashism is okay
-  till_cursor="${COMP_LINE:0:${COMP_POINT}}"  # @WARNING: Bashism
-  cursor="${till_cursor##* }"
-  set -- ${till_cursor}
-  #notify.sh "${till_cursor} ${COMP_TYPE} ${COMP_POINT} ${count} $#:|${cursor}|"
+  till_curr="${COMP_LINE:0:${COMP_POINT}}"  # @WARNING: Bashism
+  curr="${till_curr##* }"
+  set -- ${till_curr}
+  notify.sh "${till_curr} ${COMP_TYPE} ${COMP_POINT} ${count} $#:|${curr}|"
 
-  # `$0 <tab> `  First tab or on menu, empty cursor word
-  if [ "${COMP_TYPE}" != 63 ] && [ -z "${cursor}" ]; then
-    # If previous option accepts arguments, then only do file completions
-    if [ "$#" = 1 ] && [ "${_CMDS# }" != " " ]
-      then for c in ${_CMDS}; do printf %s\\n "${c}"; done
-      else compgen -A file -- ""  # ${cursor} is "", file completions
-    fi
-
-  # `$0 b<tab>` First tab, non-empty cursor word
-  elif [ "${COMP_TYPE}" = 64 ]; then  # TAB, first tab
-    out="$( [ "$#" = 2 ] && for c in ${_CMDS}; do
-      [ "${c}" != "${c#"${cursor}"}" ] && printf %s\\n "${c}"
-    done )"
-    if   [ -n "${out}" ]; then     printf %s\\n "${out}"
-    elif [ -n "${cursor}" ]; then  compgen -A file -- "${cursor}"
-    fi
-  fi
-
+  # Do not spam completion print after the first tab
+  if [ "${COMP_TYPE}" != 63 ]; then case "$#_${curr}"
+    in 1_)    printf %s\\n "${_HOSTS}"
+    ;; 2_?*)  for h in ${_HOSTS}; do
+                [ "${h}" != "${h#"${curr}"}" ] && printf %s\\n "${h}"
+              done
+    #;; 2_)    compgen -A file -- ""  # ${curr} is ""
+    #;; 3_?*)  compgen -A file -- "${curr}"
+  esac; fi
   exit 0
 fi
 
